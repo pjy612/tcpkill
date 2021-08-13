52782e4b3789ff2f12a186f39e835e96a72f4596
pengjiayi276 <pengjiayi276@pahx.com>
2021-08-13 14:38:28 +0800

    添加项目文件。

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ProcessViewer;
+
+namespace tcpkill
+{
+    class Program
+    {
+        [Flags]
+        public enum MyEnum:long
+        {
+            TEST = 1125900090893759
+        }
+        static void Main(string[] args)
+        {
+            if (args.Length >= 1)
+            {
+                if (int.TryParse(args[0], out int port))
+                {
+                    NetProcessAPI.CloseConnByLocalPort(port);
+                }
+            }
+        }
+    }
+}
